LMODroid-TMP/platform_frameworks_lmodroid

core: Import Lineage GlobalActions service
Change-Id: I4a0e5823b1ca48e235ddd4add95a6e342e754857

core/java/com/libremobileos/app/ILineageGlobalActions.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.app;
+
+/** @hide */
+interface ILineageGlobalActions {
+
+    void updateUserConfig(boolean enabled, String action);
+
+    List<String> getLocalUserConfig();
+
+    String[] getUserActionsArray();
+
+    boolean userConfigContains(String preference);
+}

core/java/com/libremobileos/app/LineageGlobalActions.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.app;
+
+import java.util.List;
+
+import android.content.Context;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.Log;
+
+public class LineageGlobalActions {
+
+    private Context mContext;
+
+    private static ILineageGlobalActions sService;
+    private static LineageGlobalActions sLineageGlobalActionsInstance;
+
+    private static final String TAG = "LineageGlobalActions";
+
+    private LineageGlobalActions(Context context) {
+        Context appContext = context.getApplicationContext();
+        if (appContext != null) {
+            mContext = appContext;
+        } else {
+            mContext = context;
+        }
+
+        try {
+            sService = getService();
+        } catch (RemoteException e) {
+            sService = null;
+        }
+
+        if (sService == null) {
+            Log.wtf(TAG, "Unable to get LineageGlobalActionsService. The service either" +
+                    " crashed, was not started, or the interface has been called to early in" +
+                    " SystemServer init");
+        }
+    }
+
+    /**
+     * Get or create an instance of the {@link lineageos.app.LineageGlobalActions}
+     * @param context
+     * @return {@link LineageGlobalActions}
+     */
+    public static LineageGlobalActions getInstance(Context context) {
+        if (sLineageGlobalActionsInstance == null) {
+            sLineageGlobalActionsInstance = new LineageGlobalActions(context);
+        }
+        return sLineageGlobalActionsInstance;
+    }
+
+    /** @hide */
+    static public ILineageGlobalActions getService() throws RemoteException {
+        if (sService != null) {
+            return sService;
+        }
+        IBinder b = ServiceManager.getService(
+                LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE);
+        sService = ILineageGlobalActions.Stub.asInterface(b);
+
+        if (sService == null) {
+            throw new RemoteException("Couldn't get " +
+                    LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE +
+                    " on binder");
+        }
+        return sService;
+    }
+
+
+    /**
+     * Update the action to the state.
+     * @param enabled a {@link Boolean} value
+     * @param action a {@link String} value
+     */
+    public void updateUserConfig(boolean enabled, String action) {
+        try {
+            getService().updateUserConfig(enabled, action);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Get the user configuration as {@link List<String>}.
+     * @return {@link List<String>}
+     */
+    public List<String> getLocalUserConfig() {
+        try {
+            return getService().getLocalUserConfig();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Get the user configuration as {@link String[]} in the same order as in the power menu.
+     * Actions are separated with | delimiter.
+     * @return {@link String[]}
+     */
+    public String[] getUserActionsArray() {
+        try {
+            return getService().getUserActionsArray();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Check if user configuration ({@link List<String>}) contains
+     * preference ({@link String})
+     * @param preference {@link String}
+     * @return {@link boolean}
+     */
+    public boolean userConfigContains(String preference) {
+        try {
+            return getService().userConfigContains(preference);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+}

services/java/com/libremobileos/server/app/LineageGlobalActionsService.java
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.server.app;
+
+import static com.libremobileos.util.PowerMenuConstants.GLOBAL_ACTION_KEY_BUGREPORT;
+import static com.libremobileos.util.PowerMenuConstants.GLOBAL_ACTION_KEY_LOCKDOWN;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.provider.Settings;
+import com.android.server.SystemService;
+
+import com.libremobileos.app.LineageContextConstants;
+import com.libremobileos.app.ILineageGlobalActions;
+import com.libremobileos.providers.LMOSettings;
+import com.libremobileos.util.PowerMenuConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @hide
+ */
+public class LineageGlobalActionsService extends SystemService {
+
+    private static final String TAG = "LineageGlobalActions";
+
+    private final Context mContext;
+    private final ContentResolver mContentResolver;
+
+    private final List<String> mLocalUserConfig = new ArrayList<String>();
+
+    // Observes user-controlled settings
+    private GlobalActionsSettingsObserver mObserver;
+
+    public LineageGlobalActionsService(Context context) {
+        super(context);
+
+        mContext = context;
+        mContentResolver = mContext.getContentResolver();
+    }
+
+    private class GlobalActionsSettingsObserver extends ContentObserver {
+
+        private final Uri BUGREPORT_URI =
+                Settings.Global.getUriFor(Settings.Global.BUGREPORT_IN_POWER_MENU);
+
+        public GlobalActionsSettingsObserver(Context context, Handler handler) {
+            super(handler);
+        }
+
+        public void observe(boolean enabled) {
+            if (enabled) {
+                mContentResolver.registerContentObserver(BUGREPORT_URI, false, this);
+            } else {
+                mContentResolver.unregisterContentObserver(this);
+            }
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateUserConfigInternal(Settings.Global.getInt(mContentResolver,
+                    Settings.Global.BUGREPORT_IN_POWER_MENU, 0) == 1,
+                    GLOBAL_ACTION_KEY_BUGREPORT);
+        }
+    };
+
+    private void populateUserConfig() {
+        mLocalUserConfig.clear();
+        mLocalUserConfig.addAll(Arrays.asList(getUserConfig()));
+    }
+
+    private String[] getUserConfig() {
+        String savedActions = Settings.Secure.getStringForUser(mContentResolver,
+                LMOSettings.Secure.POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+
+        if (savedActions == null) {
+            return mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList);
+        } else {
+            return savedActions.split("\\|");
+        }
+    }
+
+    private void updateUserConfigInternal(boolean enabled, String action) {
+        if (enabled) {
+            if (!mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.add(action);
+            }
+        } else {
+            if (mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.remove(action);
+            }
+        }
+        saveUserConfig();
+    }
+
+    private void saveUserConfig() {
+        List<String> actions = new ArrayList<String>();
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (mLocalUserConfig.contains(action)) {
+                actions.add(action);
+            }
+        }
+
+        String s = String.join("|", actions);
+        Settings.Secure.putStringForUser(mContentResolver, LMOSettings.Secure.POWER_MENU_ACTIONS, s, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public void onStart() {
+        publishBinderService(LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE, mBinder);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            populateUserConfig();
+
+            mObserver = new GlobalActionsSettingsObserver(mContext, null);
+            mObserver.observe(true);
+        }
+    }
+
+    private final IBinder mBinder = new ILineageGlobalActions.Stub() {
+
+        @Override
+        public void updateUserConfig(boolean enabled, String action) {
+            updateUserConfigInternal(enabled, action);
+        }
+
+        @Override
+        public List<String> getLocalUserConfig() {
+            populateUserConfig();
+            return mLocalUserConfig;
+        }
+
+        @Override
+        public String[] getUserActionsArray() {
+            return getUserConfig();
+        }
+
+        @Override
+        public boolean userConfigContains(String preference) {
+            return getLocalUserConfig().contains(preference);
+        }
+    };
+}

NurKeinNeid/frameworks_derpfest

core/java/org/derpfest/app/ILineageGlobalActions.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.app;
+
+/** @hide */
+interface ILineageGlobalActions {
+
+    void updateUserConfig(boolean enabled, String action);
+
+    List<String> getLocalUserConfig();
+
+    String[] getUserActionsArray();
+
+    boolean userConfigContains(String preference);
+}

core/java/org/derpfest/app/LineageGlobalActions.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.app;
+
+import java.util.List;
+
+import android.content.Context;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.Log;
+
+public class LineageGlobalActions {
+
+    private Context mContext;
+
+    private static ILineageGlobalActions sService;
+    private static LineageGlobalActions sLineageGlobalActionsInstance;
+
+    private static final String TAG = "LineageGlobalActions";
+
+    private LineageGlobalActions(Context context) {
+        Context appContext = context.getApplicationContext();
+        if (appContext != null) {
+            mContext = appContext;
+        } else {
+            mContext = context;
+        }
+
+        try {
+            sService = getService();
+        } catch (RemoteException e) {
+            sService = null;
+        }
+
+        if (sService == null) {
+            Log.wtf(TAG, "Unable to get LineageGlobalActionsService. The service either" +
+                    " crashed, was not started, or the interface has been called to early in" +
+                    " SystemServer init");
+        }
+    }
+
+    /**
+     * Get or create an instance of the {@link lineageos.app.LineageGlobalActions}
+     * @param context
+     * @return {@link LineageGlobalActions}
+     */
+    public static LineageGlobalActions getInstance(Context context) {
+        if (sLineageGlobalActionsInstance == null) {
+            sLineageGlobalActionsInstance = new LineageGlobalActions(context);
+        }
+        return sLineageGlobalActionsInstance;
+    }
+
+    /** @hide */
+    static public ILineageGlobalActions getService() throws RemoteException {
+        if (sService != null) {
+            return sService;
+        }
+        IBinder b = ServiceManager.getService(
+                LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE);
+        sService = ILineageGlobalActions.Stub.asInterface(b);
+
+        if (sService == null) {
+            throw new RemoteException("Couldn't get " +
+                    LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE +
+                    " on binder");
+        }
+        return sService;
+    }
+
+
+    /**
+     * Update the action to the state.
+     * @param enabled a {@link Boolean} value
+     * @param action a {@link String} value
+     */
+    public void updateUserConfig(boolean enabled, String action) {
+        try {
+            getService().updateUserConfig(enabled, action);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Get the user configuration as {@link List<String>}.
+     * @return {@link List<String>}
+     */
+    public List<String> getLocalUserConfig() {
+        try {
+            return getService().getLocalUserConfig();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Get the user configuration as {@link String[]} in the same order as in the power menu.
+     * Actions are separated with | delimiter.
+     * @return {@link String[]}
+     */
+    public String[] getUserActionsArray() {
+        try {
+            return getService().getUserActionsArray();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Check if user configuration ({@link List<String>}) contains
+     * preference ({@link String})
+     * @param preference {@link String}
+     * @return {@link boolean}
+     */
+    public boolean userConfigContains(String preference) {
+        try {
+            return getService().userConfigContains(preference);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+}

services/java/org/derpfest/server/app/LineageGlobalActionsService.java
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.server.app;
+
+import static org.derpfest.util.PowerMenuConstants.GLOBAL_ACTION_KEY_BUGREPORT;
+import static org.derpfest.util.PowerMenuConstants.GLOBAL_ACTION_KEY_LOCKDOWN;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.provider.Settings;
+import com.android.server.SystemService;
+
+import org.derpfest.app.LineageContextConstants;
+import org.derpfest.app.ILineageGlobalActions;
+import org.derpfest.providers.DerpFestSettings;
+import org.derpfest.util.PowerMenuConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @hide
+ */
+public class LineageGlobalActionsService extends SystemService {
+
+    private static final String TAG = "LineageGlobalActions";
+
+    private final Context mContext;
+    private final ContentResolver mContentResolver;
+
+    private final List<String> mLocalUserConfig = new ArrayList<String>();
+
+    // Observes user-controlled settings
+    private GlobalActionsSettingsObserver mObserver;
+
+    public LineageGlobalActionsService(Context context) {
+        super(context);
+
+        mContext = context;
+        mContentResolver = mContext.getContentResolver();
+    }
+
+    private class GlobalActionsSettingsObserver extends ContentObserver {
+
+        private final Uri BUGREPORT_URI =
+                Settings.Global.getUriFor(Settings.Global.BUGREPORT_IN_POWER_MENU);
+
+        public GlobalActionsSettingsObserver(Context context, Handler handler) {
+            super(handler);
+        }
+
+        public void observe(boolean enabled) {
+            if (enabled) {
+                mContentResolver.registerContentObserver(BUGREPORT_URI, false, this);
+            } else {
+                mContentResolver.unregisterContentObserver(this);
+            }
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateUserConfigInternal(Settings.Global.getInt(mContentResolver,
+                    Settings.Global.BUGREPORT_IN_POWER_MENU, 0) == 1,
+                    GLOBAL_ACTION_KEY_BUGREPORT);
+        }
+    };
+
+    private void populateUserConfig() {
+        mLocalUserConfig.clear();
+        mLocalUserConfig.addAll(Arrays.asList(getUserConfig()));
+    }
+
+    private String[] getUserConfig() {
+        String savedActions = Settings.Secure.getStringForUser(mContentResolver,
+                DerpFestSettings.Secure.POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+
+        if (savedActions == null) {
+            return mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList);
+        } else {
+            return savedActions.split("\\|");
+        }
+    }
+
+    private void updateUserConfigInternal(boolean enabled, String action) {
+        if (enabled) {
+            if (!mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.add(action);
+            }
+        } else {
+            if (mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.remove(action);
+            }
+        }
+        saveUserConfig();
+    }
+
+    private void saveUserConfig() {
+        List<String> actions = new ArrayList<String>();
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (mLocalUserConfig.contains(action)) {
+                actions.add(action);
+            }
+        }
+
+        String s = String.join("|", actions);
+        Settings.Secure.putStringForUser(mContentResolver, DerpFestSettings.Secure.POWER_MENU_ACTIONS, s, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public void onStart() {
+        publishBinderService(LineageContextConstants.LINEAGE_GLOBAL_ACTIONS_SERVICE, mBinder);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            populateUserConfig();
+
+            mObserver = new GlobalActionsSettingsObserver(mContext, null);
+            mObserver.observe(true);
+        }
+    }
+
+    private final IBinder mBinder = new ILineageGlobalActions.Stub() {
+
+        @Override
+        public void updateUserConfig(boolean enabled, String action) {
+            updateUserConfigInternal(enabled, action);
+        }
+
+        @Override
+        public List<String> getLocalUserConfig() {
+            populateUserConfig();
+            return mLocalUserConfig;
+        }
+
+        @Override
+        public String[] getUserActionsArray() {
+            return getUserConfig();
+        }
+
+        @Override
+        public boolean userConfigContains(String preference) {
+            return getLocalUserConfig().contains(preference);
+        }
+    };
+}