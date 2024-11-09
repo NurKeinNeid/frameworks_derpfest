303d5da363968e52e4c1eb2b35b8e2b408bdbbb5
NurKeinNeid <mralexman3000@gmail.com>
2024-11-09 20:59:10 +0100

    framework: Add restart systemui shortcut to power menu [1.5/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/org/derpfest/util/PowerMenuConstants.java
+++ core/java/org/derpfest/util/PowerMenuConstants.java
@@ -34,6 +34,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_EMERGENCY = "emergency";
     public static final String GLOBAL_ACTION_KEY_DEVICECONTROLS = "devicecontrols";
     public static final String GLOBAL_ACTION_KEY_PANIC = "panic";
+    public static final String GLOBAL_ACTION_KEY_RESTART_SYSTEMUI = "restart_systemui";
     public static final String GLOBAL_ACTION_KEY_SYSTEM_UPDATE = "system_update";
 
     /**
@@ -66,6 +67,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_ASSIST,
         GLOBAL_ACTION_KEY_DEVICECONTROLS,
         GLOBAL_ACTION_KEY_PANIC,
+        GLOBAL_ACTION_KEY_RESTART_SYSTEMUI,
         GLOBAL_ACTION_KEY_LOGOUT,
         GLOBAL_ACTION_KEY_SYSTEM_UPDATE,
     };
