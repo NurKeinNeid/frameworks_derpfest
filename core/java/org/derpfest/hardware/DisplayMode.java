LMODroid-TMP/platform_frameworks_lmodroid

core: Introduce LiveDisplay from Lineage
Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d

core/java/com/libremobileos/hardware/DisplayMode.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.libremobileos.os.Concierge;
+import com.libremobileos.os.Concierge.ParcelInfo;
+
+/**
+ * Display Modes API
+ *
+ * A device may implement a list of preset display modes for different
+ * viewing intents, such as movies, photos, or extra vibrance. These
+ * modes may have multiple components such as gamma correction, white
+ * point adjustment, etc, but are activated by a single control point.
+ *
+ * This API provides support for enumerating and selecting the
+ * modes supported by the hardware.
+ *
+ * A DisplayMode is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class DisplayMode implements Parcelable {
+    public final int id;
+    public final String name;
+
+    public DisplayMode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private DisplayMode(Parcel parcel) {
+        // Read parcelable version via the Concierge
+        ParcelInfo parcelInfo = Concierge.receiveParcel(parcel);
+
+        // temp vars
+        int tmpId = -1;
+        String tmpName = null;
+
+        tmpId = parcel.readInt();
+        if (parcel.readInt() != 0) {
+            tmpName = parcel.readString();
+        }
+
+        // set temps
+        this.id = tmpId;
+        this.name = tmpName;
+
+        // Complete parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        // Tell the concierge to prepare the parcel
+        ParcelInfo parcelInfo = Concierge.prepareParcel(out);
+
+        // ==== BOYSENBERRY =====
+        out.writeInt(id);
+        if (name != null) {
+            out.writeInt(1);
+            out.writeString(name);
+        } else {
+            out.writeInt(0);
+        }
+
+        // Complete the parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    /** @hide */
+    public static final Parcelable.Creator<DisplayMode> CREATOR =
+            new Parcelable.Creator<DisplayMode>() {
+        public DisplayMode createFromParcel(Parcel in) {
+            return new DisplayMode(in);
+        }
+
+        @Override
+        public DisplayMode[] newArray(int size) {
+            return new DisplayMode[size];
+        }
+    };
+
+}

core/java/com/libremobileos/hardware/ILiveDisplayService.aidl
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2016, The CyanogenMod Project
+ *               2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.hardware;
+
+import com.libremobileos.hardware.HSIC;
+import com.libremobileos.hardware.LiveDisplayConfig;
+
+/** @hide */
+interface ILiveDisplayService {
+    LiveDisplayConfig getConfig();
+
+    int getMode();
+    boolean setMode(int mode);
+
+    float[] getColorAdjustment();
+    boolean setColorAdjustment(in float[] adj);
+
+    boolean isAutoContrastEnabled();
+    boolean setAutoContrastEnabled(boolean enabled);
+
+    boolean isCABCEnabled();
+    boolean setCABCEnabled(boolean enabled);
+
+    boolean isColorEnhancementEnabled();
+    boolean setColorEnhancementEnabled(boolean enabled);
+
+    int getDayColorTemperature();
+    boolean setDayColorTemperature(int temperature);
+
+    int getNightColorTemperature();
+    boolean setNightColorTemperature(int temperature);
+
+    int getColorTemperature();
+
+    boolean isAutomaticOutdoorModeEnabled();
+    boolean setAutomaticOutdoorModeEnabled(boolean enabled);
+
+    HSIC getPictureAdjustment();
+    HSIC getDefaultPictureAdjustment();
+    boolean setPictureAdjustment(in HSIC adj);
+    boolean isNight();
+
+    boolean isAntiFlickerEnabled();
+    boolean setAntiFlickerEnabled(boolean enabled);
+}

core/java/com/libremobileos/util/MathUtils.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.util;
+
+public final class MathUtils {
+
+    /**
+     * Given a range of values which change continuously in a non-linear way,
+     * we can map back and forth to a linear scale using some quadratic equations.
+     *
+     * The linear scale ranges from 0 -> 1. This method will calculate the
+     * coefficients needed to solve the conversion functions in the next two methods.
+     *
+     * lower = actual value when linear value = 0
+     * mid = actual value when linear value = .5
+     * upper actual value when linear value = 1
+     *
+     * @param lower
+     * @param mid
+     * @param upper
+     * @return array of coefficients
+     */
+    public static double[] powerCurve(double lower, double mid, double upper) {
+        final double[] curve = new double[3];
+        curve[0] = ((lower * upper) - (mid * mid)) / (lower - (2 * mid) + upper);
+        curve[1] = Math.pow((mid - lower), 2) / (lower - (2 * mid) + upper);
+        curve[2] = 2 * Math.log((upper - mid) / (mid - lower));
+        return curve;
+    }
+
+    /**
+     * Map a value on a power curve to a linear value
+     *
+     * @param curve obtained from powerCurve()
+     * @param value to convert to linear scale
+     * @return linear value from 0 -> 1
+     */
+    public static double powerCurveToLinear(final double[] curve, double value) {
+        return Math.log((value - curve[0]) / curve[1]) / curve[2];
+    }
+
+    /**
+     * Map a value on a linear scale to a value on a power curve
+     *
+     * @param curve obtained from powerCurve()
+     * @param value from 0 -> 1 to map onto power curve
+     * @return actual value on the given curve
+     */
+    public static double linearToPowerCurve(final double[] curve, double value) {
+        return curve[0] + curve[1] * Math.exp(curve[2] * value);
+    }
+
+
+}

NurKeinNeid/frameworks_derpfest

core/java/org/derpfest/hardware/DisplayMode.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.derpfest.os.Concierge;
+import org.derpfest.os.Concierge.ParcelInfo;
+
+/**
+ * Display Modes API
+ *
+ * A device may implement a list of preset display modes for different
+ * viewing intents, such as movies, photos, or extra vibrance. These
+ * modes may have multiple components such as gamma correction, white
+ * point adjustment, etc, but are activated by a single control point.
+ *
+ * This API provides support for enumerating and selecting the
+ * modes supported by the hardware.
+ *
+ * A DisplayMode is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class DisplayMode implements Parcelable {
+    public final int id;
+    public final String name;
+
+    public DisplayMode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private DisplayMode(Parcel parcel) {
+        // Read parcelable version via the Concierge
+        ParcelInfo parcelInfo = Concierge.receiveParcel(parcel);
+
+        // temp vars
+        int tmpId = -1;
+        String tmpName = null;
+
+        tmpId = parcel.readInt();
+        if (parcel.readInt() != 0) {
+            tmpName = parcel.readString();
+        }
+
+        // set temps
+        this.id = tmpId;
+        this.name = tmpName;
+
+        // Complete parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        // Tell the concierge to prepare the parcel
+        ParcelInfo parcelInfo = Concierge.prepareParcel(out);
+
+        // ==== BOYSENBERRY =====
+        out.writeInt(id);
+        if (name != null) {
+            out.writeInt(1);
+            out.writeString(name);
+        } else {
+            out.writeInt(0);
+        }
+
+        // Complete the parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    /** @hide */
+    public static final Parcelable.Creator<DisplayMode> CREATOR =
+            new Parcelable.Creator<DisplayMode>() {
+        public DisplayMode createFromParcel(Parcel in) {
+            return new DisplayMode(in);
+        }
+
+        @Override
+        public DisplayMode[] newArray(int size) {
+            return new DisplayMode[size];
+        }
+    };
+
+}

core/java/org/derpfest/hardware/ILiveDisplayService.aidl
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2016, The CyanogenMod Project
+ *               2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.hardware;
+
+import org.derpfest.hardware.HSIC;
+import org.derpfest.hardware.LiveDisplayConfig;
+
+/** @hide */
+interface ILiveDisplayService {
+    LiveDisplayConfig getConfig();
+
+    int getMode();
+    boolean setMode(int mode);
+
+    float[] getColorAdjustment();
+    boolean setColorAdjustment(in float[] adj);
+
+    boolean isAutoContrastEnabled();
+    boolean setAutoContrastEnabled(boolean enabled);
+
+    boolean isCABCEnabled();
+    boolean setCABCEnabled(boolean enabled);
+
+    boolean isColorEnhancementEnabled();
+    boolean setColorEnhancementEnabled(boolean enabled);
+
+    int getDayColorTemperature();
+    boolean setDayColorTemperature(int temperature);
+
+    int getNightColorTemperature();
+    boolean setNightColorTemperature(int temperature);
+
+    int getColorTemperature();
+
+    boolean isAutomaticOutdoorModeEnabled();
+    boolean setAutomaticOutdoorModeEnabled(boolean enabled);
+
+    HSIC getPictureAdjustment();
+    HSIC getDefaultPictureAdjustment();
+    boolean setPictureAdjustment(in HSIC adj);
+    boolean isNight();
+
+    boolean isAntiFlickerEnabled();
+    boolean setAntiFlickerEnabled(boolean enabled);
+}

core/java/org/derpfest/util/MathUtils.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.derpfest.util;
+
+public final class MathUtils {
+
+    /**
+     * Given a range of values which change continuously in a non-linear way,
+     * we can map back and forth to a linear scale using some quadratic equations.
+     *
+     * The linear scale ranges from 0 -> 1. This method will calculate the
+     * coefficients needed to solve the conversion functions in the next two methods.
+     *
+     * lower = actual value when linear value = 0
+     * mid = actual value when linear value = .5
+     * upper actual value when linear value = 1
+     *
+     * @param lower
+     * @param mid
+     * @param upper
+     * @return array of coefficients
+     */
+    public static double[] powerCurve(double lower, double mid, double upper) {
+        final double[] curve = new double[3];
+        curve[0] = ((lower * upper) - (mid * mid)) / (lower - (2 * mid) + upper);
+        curve[1] = Math.pow((mid - lower), 2) / (lower - (2 * mid) + upper);
+        curve[2] = 2 * Math.log((upper - mid) / (mid - lower));
+        return curve;
+    }
+
+    /**
+     * Map a value on a power curve to a linear value
+     *
+     * @param curve obtained from powerCurve()
+     * @param value to convert to linear scale
+     * @return linear value from 0 -> 1
+     */
+    public static double powerCurveToLinear(final double[] curve, double value) {
+        return Math.log((value - curve[0]) / curve[1]) / curve[2];
+    }
+
+    /**
+     * Map a value on a linear scale to a value on a power curve
+     *
+     * @param curve obtained from powerCurve()
+     * @param value from 0 -> 1 to map onto power curve
+     * @return actual value on the given curve
+     */
+    public static double linearToPowerCurve(final double[] curve, double value) {
+        return curve[0] + curve[1] * Math.exp(curve[2] * value);
+    }
+
+
+}