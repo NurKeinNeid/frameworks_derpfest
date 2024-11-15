LMODroid-TMP/platform_frameworks_lmodroid

framework: Add applock
Change-Id: I35fda3a018893903b2af1daa2f57df1c2246e12c

core/java/com/libremobileos/applock/AppLockData.aidl
@@ -1,0 +1,19 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.applock;
+
+parcelable AppLockData;

core/java/com/libremobileos/applock/AppLockData.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.applock;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Class to hold package level information about an
+ * application protected with app lock.
+ *
+ * @hide
+ */
+public final class AppLockData implements Parcelable {
+
+    public static final Parcelable.Creator<AppLockData> CREATOR =
+            new Parcelable.Creator<AppLockData>() {
+
+        @Override
+        public AppLockData createFromParcel(Parcel in) {
+            return new AppLockData(in);
+        }
+
+        @Override
+        public AppLockData[] newArray(int size) {
+            return new AppLockData[size];
+        }
+    };
+
+    private final String mPackageName;
+    private final boolean mShouldRedactNotification;
+    private final boolean mHideFromLauncher;
+
+    /** @hide */
+    public AppLockData(
+        @NonNull final String packageName,
+        final boolean shouldRedactNotification,
+        final boolean hideFromLauncher
+    ) {
+        mPackageName = packageName;
+        mShouldRedactNotification = shouldRedactNotification;
+        mHideFromLauncher = hideFromLauncher;
+    }
+
+    private AppLockData(final Parcel in) {
+        mPackageName = in.readString();
+        mShouldRedactNotification = in.readBoolean();
+        mHideFromLauncher = in.readBoolean();
+    }
+
+    @NonNull
+    public String getPackageName() {
+        return mPackageName;
+    }
+
+    public boolean getShouldRedactNotification() {
+        return mShouldRedactNotification;
+    }
+
+    public boolean getHideFromLauncher() {
+        return mHideFromLauncher;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel parcel, final int flags) {
+        parcel.writeString(mPackageName);
+        parcel.writeBoolean(mShouldRedactNotification);
+        parcel.writeBoolean(mHideFromLauncher);
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "AppLockData[ packageName = " + mPackageName +
+            ", shouldRedactNotification = " + mShouldRedactNotification +
+            ", hideFromLauncher = " + mHideFromLauncher + " ]";
+    }
+}

core/java/com/libremobileos/applock/IAppLockManagerService.aidl
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.applock;
+
+import com.libremobileos.applock.AppLockData;
+
+/**
+ * Interface for managing app lock.
+ * @hide
+ */
+interface IAppLockManagerService {
+
+    void addPackage(in String packageName, in int userId);
+
+    void removePackage(in String packageName, in int userId);
+
+    long getTimeout(in int userId);
+
+    void setTimeout(in long timeout, in int userId);
+
+    List<AppLockData> getPackageData(in int userId);
+
+    void setShouldRedactNotification(in String packageName, in boolean secure, in int userId);
+
+    void setBiometricsAllowed(in boolean biometricsAllowed, in int userId);
+
+    boolean isBiometricsAllowed(in int userId);
+
+    void unlockPackage(in String packageName, in int userId);
+
+    void setPackageHidden(in String packageName, boolean hide, in int userId);
+
+    List<String> getHiddenPackages(in int userId);
+}

services/java/com/libremobileos/server/applock/BiometricUnlocker.kt
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.server.applock
+
+import android.content.Context
+import android.hardware.biometrics.BiometricConstants
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators
+import android.hardware.biometrics.BiometricPrompt
+import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback
+import android.hardware.biometrics.BiometricPrompt.AuthenticationResult
+import android.os.CancellationSignal
+import android.util.Slog
+
+import com.android.internal.R
+
+/**
+ * Handles logic of unlocking an app with biometrics or device credentials.
+ *
+ * @hide
+ */
+internal class BiometricUnlocker(private val context: Context) {
+
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    // Set operation must be externally synchronized
+    var biometricsAllowed = false
+
+    /**
+     * Determine whether biometrics or device credentials can be used for
+     * unlocking operation.
+     */
+    fun canUnlock(): Boolean =
+        biometricManager.canAuthenticate(
+            Authenticators.BIOMETRIC_STRONG or
+                Authenticators.DEVICE_CREDENTIAL
+        ) == BiometricManager.BIOMETRIC_SUCCESS
+
+    /**
+     * Unlock an application. Should call this method only if
+     * [canUnlock] returned true.
+     *
+     * @param title the title of the dialog prompt.
+     * @param onSuccess the callback invoked on successfull authentication.
+     * @param onCancel the callback invoked when authentication is cancelled.
+     */
+    fun unlock(
+        packageLabel: String?,
+        onSuccess: () -> Unit,
+        onCancel: () -> Unit,
+    ) {
+        val callback = object : AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: AuthenticationResult) {
+                logD {
+                    "onAuthenticationSucceeded"
+                }
+                onSuccess()
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                Slog.i(TAG, "onAuthenticationError, errorCode = " +
+                    "$errorCode, errString = $errString")
+                if (errorCode == BiometricConstants.BIOMETRIC_ERROR_USER_CANCELED) {
+                    onCancel()
+                }
+            }
+        }
+        showCredentialsPrompt(
+            context.getString(R.string.unlock_application, packageLabel),
+            callback
+        )
+    }
+
+    private fun showCredentialsPrompt(
+        title: String,
+        callback: AuthenticationCallback,
+    ) {
+        var authenticators = Authenticators.DEVICE_CREDENTIAL
+        if (biometricsAllowed) {
+            authenticators = authenticators or Authenticators.BIOMETRIC_STRONG
+        }
+        val prompt = BiometricPrompt.Builder(context)
+            .setTitle(title)
+            .setAllowedAuthenticators(authenticators)
+            .setAllowBackgroundAuthentication(true)
+            .build()
+        prompt.authenticateUser(
+            CancellationSignal(),
+            context.mainExecutor,
+            callback,
+            context.userId,
+        )
+    }
+}

NurKeinNeid/frameworks_derpfest

core/java/org/derpfest/applock/AppLockData.aidl
@@ -1,0 +1,19 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.applock;
+
+parcelable AppLockData;

core/java/org/derpfest/applock/AppLockData.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.applock;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Class to hold package level information about an
+ * application protected with app lock.
+ *
+ * @hide
+ */
+public final class AppLockData implements Parcelable {
+
+    public static final Parcelable.Creator<AppLockData> CREATOR =
+            new Parcelable.Creator<AppLockData>() {
+
+        @Override
+        public AppLockData createFromParcel(Parcel in) {
+            return new AppLockData(in);
+        }
+
+        @Override
+        public AppLockData[] newArray(int size) {
+            return new AppLockData[size];
+        }
+    };
+
+    private final String mPackageName;
+    private final boolean mShouldRedactNotification;
+    private final boolean mHideFromLauncher;
+
+    /** @hide */
+    public AppLockData(
+        @NonNull final String packageName,
+        final boolean shouldRedactNotification,
+        final boolean hideFromLauncher
+    ) {
+        mPackageName = packageName;
+        mShouldRedactNotification = shouldRedactNotification;
+        mHideFromLauncher = hideFromLauncher;
+    }
+
+    private AppLockData(final Parcel in) {
+        mPackageName = in.readString();
+        mShouldRedactNotification = in.readBoolean();
+        mHideFromLauncher = in.readBoolean();
+    }
+
+    @NonNull
+    public String getPackageName() {
+        return mPackageName;
+    }
+
+    public boolean getShouldRedactNotification() {
+        return mShouldRedactNotification;
+    }
+
+    public boolean getHideFromLauncher() {
+        return mHideFromLauncher;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel parcel, final int flags) {
+        parcel.writeString(mPackageName);
+        parcel.writeBoolean(mShouldRedactNotification);
+        parcel.writeBoolean(mHideFromLauncher);
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "AppLockData[ packageName = " + mPackageName +
+            ", shouldRedactNotification = " + mShouldRedactNotification +
+            ", hideFromLauncher = " + mHideFromLauncher + " ]";
+    }
+}

core/java/org/derpfest/applock/IAppLockManagerService.aidl
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.applock;
+
+import org.derpfest.applock.AppLockData;
+
+/**
+ * Interface for managing app lock.
+ * @hide
+ */
+interface IAppLockManagerService {
+
+    void addPackage(in String packageName, in int userId);
+
+    void removePackage(in String packageName, in int userId);
+
+    long getTimeout(in int userId);
+
+    void setTimeout(in long timeout, in int userId);
+
+    List<AppLockData> getPackageData(in int userId);
+
+    void setShouldRedactNotification(in String packageName, in boolean secure, in int userId);
+
+    void setBiometricsAllowed(in boolean biometricsAllowed, in int userId);
+
+    boolean isBiometricsAllowed(in int userId);
+
+    void unlockPackage(in String packageName, in int userId);
+
+    void setPackageHidden(in String packageName, boolean hide, in int userId);
+
+    List<String> getHiddenPackages(in int userId);
+}

services/java/org/derpfest/server/applock/BiometricUnlocker.kt
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.server.applock
+
+import android.content.Context
+import android.hardware.biometrics.BiometricConstants
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators
+import android.hardware.biometrics.BiometricPrompt
+import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback
+import android.hardware.biometrics.BiometricPrompt.AuthenticationResult
+import android.os.CancellationSignal
+import android.util.Slog
+
+import com.android.internal.R
+
+/**
+ * Handles logic of unlocking an app with biometrics or device credentials.
+ *
+ * @hide
+ */
+internal class BiometricUnlocker(private val context: Context) {
+
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    // Set operation must be externally synchronized
+    var biometricsAllowed = false
+
+    /**
+     * Determine whether biometrics or device credentials can be used for
+     * unlocking operation.
+     */
+    fun canUnlock(): Boolean =
+        biometricManager.canAuthenticate(
+            Authenticators.BIOMETRIC_STRONG or
+                Authenticators.DEVICE_CREDENTIAL
+        ) == BiometricManager.BIOMETRIC_SUCCESS
+
+    /**
+     * Unlock an application. Should call this method only if
+     * [canUnlock] returned true.
+     *
+     * @param title the title of the dialog prompt.
+     * @param onSuccess the callback invoked on successfull authentication.
+     * @param onCancel the callback invoked when authentication is cancelled.
+     */
+    fun unlock(
+        packageLabel: String?,
+        onSuccess: () -> Unit,
+        onCancel: () -> Unit,
+    ) {
+        val callback = object : AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: AuthenticationResult) {
+                logD {
+                    "onAuthenticationSucceeded"
+                }
+                onSuccess()
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                Slog.i(TAG, "onAuthenticationError, errorCode = " +
+                    "$errorCode, errString = $errString")
+                if (errorCode == BiometricConstants.BIOMETRIC_ERROR_USER_CANCELED) {
+                    onCancel()
+                }
+            }
+        }
+        showCredentialsPrompt(
+            context.getString(R.string.unlock_application, packageLabel),
+            callback
+        )
+    }
+
+    private fun showCredentialsPrompt(
+        title: String,
+        callback: AuthenticationCallback,
+    ) {
+        var authenticators = Authenticators.DEVICE_CREDENTIAL
+        if (biometricsAllowed) {
+            authenticators = authenticators or Authenticators.BIOMETRIC_STRONG
+        }
+        val prompt = BiometricPrompt.Builder(context)
+            .setTitle(title)
+            .setAllowedAuthenticators(authenticators)
+            .setAllowBackgroundAuthentication(true)
+            .build()
+        prompt.authenticateUser(
+            CancellationSignal(),
+            context.mainExecutor,
+            callback,
+            context.userId,
+        )
+    }
+}