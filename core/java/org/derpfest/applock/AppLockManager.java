LMODroid-TMP/platform_frameworks_lmodroid

core: Add Manifest.java and unhardcode the manifest usage
Change-Id: Iad12bd3a4c94dbd18035c85ad2114ebb62f2bc95

core/java/com/libremobileos/applock/AppLockManager.java
@@ -95,7 +95,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void addPackage(@NonNull String packageName) {
         try {
             sService.addPackage(packageName, mContext.getUserId());
@@ -112,7 +112,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void removePackage(@NonNull String packageName) {
         try {
             sService.removePackage(packageName, mContext.getUserId());
@@ -147,7 +147,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void setTimeout(long timeout) {
         try {
             sService.setTimeout(timeout, mContext.getUserId());
@@ -165,7 +165,7 @@ public final class AppLockManager {
      */
     @UserHandleAware
     @NonNull
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public List<AppLockData> getPackageData() {
         try {
             return sService.getPackageData(mContext.getUserId());
@@ -183,7 +183,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void setShouldRedactNotification(@NonNull String packageName, boolean shouldRedactNotification) {
         try {
             sService.setShouldRedactNotification(packageName, shouldRedactNotification, mContext.getUserId());
@@ -200,7 +200,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void setBiometricsAllowed(boolean biometricsAllowed) {
         try {
             sService.setBiometricsAllowed(biometricsAllowed, mContext.getUserId());
@@ -232,7 +232,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void unlockPackage(@NonNull String packageName) {
         try {
             sService.unlockPackage(packageName, mContext.getUserId());
@@ -250,7 +250,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     public void setPackageHidden(@NonNull String packageName, boolean hide) {
         try {
             sService.setPackageHidden(packageName, hide, mContext.getUserId());
@@ -267,7 +267,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(com.libremobileos.Manifest.permission.MANAGE_APP_LOCK)
     @NonNull
     public List<String> getHiddenPackages() {
         try {

services/java/com/libremobileos/server/display/LiveDisplayService.java
@@ -237,7 +237,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setMode(int mode) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             if (!mConfig.hasModeSupport()) {
                 return false;
             }
@@ -252,7 +252,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setColorAdjustment(float[] adj) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setColorAdjustment(adj);
         }
 
@@ -264,7 +264,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public  boolean setAutoContrastEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setAutoContrastEnabled(enabled);
         }
 
@@ -276,7 +276,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setCABCEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setCABCEnabled(enabled);
         }
 
@@ -288,7 +288,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setColorEnhancementEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setColorEnhancementEnabled(enabled);
         }
 
@@ -300,7 +300,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setAutomaticOutdoorModeEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mOMC.setAutomaticOutdoorModeEnabled(enabled);
         }
 
@@ -312,7 +312,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setDayColorTemperature(int temperature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             mCTC.setDayColorTemperature(temperature);
             return true;
         }
@@ -325,7 +325,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setNightColorTemperature(int temperature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             mCTC.setNightColorTemperature(temperature);
             return true;
         }
@@ -373,7 +373,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setAntiFlickerEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    com.libremobileos.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setAntiFlickerEnabled(enabled);
         }
     };

services/java/com/libremobileos/server/hardware/LineageHardwareService.java
@@ -185,7 +185,7 @@ public class LineageHardwareService extends SystemService {
             Intent intent = new Intent(com.libremobileos.content.Intent.ACTION_INITIALIZE_LINEAGE_HARDWARE);
             intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
             mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS");
+                    com.libremobileos.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS);
         }
     }
 
@@ -203,14 +203,14 @@ public class LineageHardwareService extends SystemService {
         @Override
         public int getSupportedFeatures() {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    com.libremobileos.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             return mLineageHwImpl.getSupportedFeatures();
         }
 
         @Override
         public boolean get(int feature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    com.libremobileos.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             if (!isSupported(feature)) {
                 Log.e(TAG, "feature " + feature + " is not supported");
                 return false;
@@ -221,7 +221,7 @@ public class LineageHardwareService extends SystemService {
         @Override
         public boolean set(int feature, boolean enable) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    com.libremobileos.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             if (!isSupported(feature)) {
                 Log.e(TAG, "feature " + feature + " is not supported");
                 return false;

NurKeinNeid/frameworks_derpfest

core/java/org/derpfest/applock/AppLockManager.java
@@ -95,7 +95,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void addPackage(@NonNull String packageName) {
         try {
             sService.addPackage(packageName, mContext.getUserId());
@@ -112,7 +112,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void removePackage(@NonNull String packageName) {
         try {
             sService.removePackage(packageName, mContext.getUserId());
@@ -147,7 +147,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void setTimeout(long timeout) {
         try {
             sService.setTimeout(timeout, mContext.getUserId());
@@ -165,7 +165,7 @@ public final class AppLockManager {
      */
     @UserHandleAware
     @NonNull
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public List<AppLockData> getPackageData() {
         try {
             return sService.getPackageData(mContext.getUserId());
@@ -183,7 +183,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void setShouldRedactNotification(@NonNull String packageName, boolean shouldRedactNotification) {
         try {
             sService.setShouldRedactNotification(packageName, shouldRedactNotification, mContext.getUserId());
@@ -200,7 +200,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void setBiometricsAllowed(boolean biometricsAllowed) {
         try {
             sService.setBiometricsAllowed(biometricsAllowed, mContext.getUserId());
@@ -232,7 +232,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void unlockPackage(@NonNull String packageName) {
         try {
             sService.unlockPackage(packageName, mContext.getUserId());
@@ -250,7 +250,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     public void setPackageHidden(@NonNull String packageName, boolean hide) {
         try {
             sService.setPackageHidden(packageName, hide, mContext.getUserId());
@@ -267,7 +267,7 @@ public final class AppLockManager {
      * @hide
      */
     @UserHandleAware
-    @RequiresPermission(Manifest.permission.MANAGE_APP_LOCK)
+    @RequiresPermission(org.derpfest.Manifest.permission.MANAGE_APP_LOCK)
     @NonNull
     public List<String> getHiddenPackages() {
         try {

services/java/org/derpfest/server/display/LiveDisplayService.java
@@ -237,7 +237,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setMode(int mode) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             if (!mConfig.hasModeSupport()) {
                 return false;
             }
@@ -252,7 +252,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setColorAdjustment(float[] adj) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setColorAdjustment(adj);
         }
 
@@ -264,7 +264,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public  boolean setAutoContrastEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setAutoContrastEnabled(enabled);
         }
 
@@ -276,7 +276,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setCABCEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setCABCEnabled(enabled);
         }
 
@@ -288,7 +288,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setColorEnhancementEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setColorEnhancementEnabled(enabled);
         }
 
@@ -300,7 +300,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setAutomaticOutdoorModeEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mOMC.setAutomaticOutdoorModeEnabled(enabled);
         }
 
@@ -312,7 +312,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setDayColorTemperature(int temperature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             mCTC.setDayColorTemperature(temperature);
             return true;
         }
@@ -325,7 +325,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setNightColorTemperature(int temperature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             mCTC.setNightColorTemperature(temperature);
             return true;
         }
@@ -373,7 +373,7 @@ public class LiveDisplayService extends SystemService {
         @Override
         public boolean setAntiFlickerEnabled(boolean enabled) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.MANAGE_LIVEDISPLAY", null);
+                    org.derpfest.Manifest.permission.MANAGE_LIVEDISPLAY, null);
             return mDHC.setAntiFlickerEnabled(enabled);
         }
     };

services/java/org/derpfest/server/hardware/LineageHardwareService.java
@@ -185,7 +185,7 @@ public class LineageHardwareService extends SystemService {
             Intent intent = new Intent(org.derpfest.content.Intent.ACTION_INITIALIZE_LINEAGE_HARDWARE);
             intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
             mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS");
+                    org.derpfest.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS);
         }
     }
 
@@ -203,14 +203,14 @@ public class LineageHardwareService extends SystemService {
         @Override
         public int getSupportedFeatures() {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    org.derpfest.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             return mLineageHwImpl.getSupportedFeatures();
         }
 
         @Override
         public boolean get(int feature) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    org.derpfest.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             if (!isSupported(feature)) {
                 Log.e(TAG, "feature " + feature + " is not supported");
                 return false;
@@ -221,7 +221,7 @@ public class LineageHardwareService extends SystemService {
         @Override
         public boolean set(int feature, boolean enable) {
             mContext.enforceCallingOrSelfPermission(
-                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+                    org.derpfest.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
             if (!isSupported(feature)) {
                 Log.e(TAG, "feature " + feature + " is not supported");
                 return false;