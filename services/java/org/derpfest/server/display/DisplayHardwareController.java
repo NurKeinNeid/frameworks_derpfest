LMODroid/platform_frameworks_lmodroid

frameworks: Optimize imports
Reordering and removing unused ones

Change-Id: I1fff9c6f11c71022dbf4207ee042d2ee2ff77ee6

services/java/com/libremobileos/server/display/DisplayHardwareController.java
@@ -30,14 +30,14 @@ import android.util.MathUtils;
 import android.util.Slog;
 import android.view.animation.LinearInterpolator;
 
+import com.libremobileos.providers.LMOSettings;
+import com.libremobileos.hardware.LineageHardwareManager;
+import com.libremobileos.hardware.LiveDisplayManager;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
-
-import com.libremobileos.providers.LMOSettings;
-import com.libremobileos.hardware.LineageHardwareManager;
-import com.libremobileos.hardware.LiveDisplayManager;
 
 public class DisplayHardwareController extends LiveDisplayFeature {
 

services/java/com/libremobileos/server/display/LiveDisplayFeature.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,11 @@
  */
 package com.libremobileos.server.display;
 
+import static com.libremobileos.server.display.LiveDisplayService.ALL_CHANGED;
+import static com.libremobileos.server.display.LiveDisplayService.DISPLAY_CHANGED;
+import static com.libremobileos.server.display.LiveDisplayService.MODE_CHANGED;
+import static com.libremobileos.server.display.LiveDisplayService.TWILIGHT_CHANGED;
+
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;
 import android.os.Handler;
@@ -26,11 +31,6 @@ import com.libremobileos.server.display.LiveDisplayService.State;
 import com.libremobileos.server.display.TwilightTracker.TwilightState;
 
 import java.util.BitSet;
-
-import static com.libremobileos.server.display.LiveDisplayService.ALL_CHANGED;
-import static com.libremobileos.server.display.LiveDisplayService.DISPLAY_CHANGED;
-import static com.libremobileos.server.display.LiveDisplayService.MODE_CHANGED;
-import static com.libremobileos.server.display.LiveDisplayService.TWILIGHT_CHANGED;
 
 public abstract class LiveDisplayFeature extends LineageBaseFeature {
 

services/java/com/libremobileos/server/display/LiveDisplayService.java
@@ -16,6 +16,10 @@
  */
 package com.libremobileos.server.display;
 
+import static com.libremobileos.hardware.LiveDisplayManager.MODE_FIRST;
+import static com.libremobileos.hardware.LiveDisplayManager.MODE_LAST;
+import static com.libremobileos.hardware.LiveDisplayManager.MODE_OFF;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -42,6 +46,12 @@ import com.libremobileos.server.common.UserContentObserver;
 import com.libremobileos.server.display.TwilightTracker.TwilightListener;
 import com.libremobileos.server.display.TwilightTracker.TwilightState;
 
+import com.libremobileos.app.LineageContextConstants;
+import com.libremobileos.providers.LMOSettings;
+import com.libremobileos.hardware.HSIC;
+import com.libremobileos.hardware.ILiveDisplayService;
+import com.libremobileos.hardware.LiveDisplayConfig;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -49,16 +59,6 @@ import java.util.BitSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-
-import com.libremobileos.app.LineageContextConstants;
-import com.libremobileos.providers.LMOSettings;
-import com.libremobileos.hardware.HSIC;
-import com.libremobileos.hardware.ILiveDisplayService;
-import com.libremobileos.hardware.LiveDisplayConfig;
-
-import static com.libremobileos.hardware.LiveDisplayManager.MODE_FIRST;
-import static com.libremobileos.hardware.LiveDisplayManager.MODE_LAST;
-import static com.libremobileos.hardware.LiveDisplayManager.MODE_OFF;
 
 /**
  * LiveDisplay is an advanced set of features for improving

services/java/com/libremobileos/server/health/ChargingControlController.java
@@ -16,6 +16,11 @@
 
 package com.libremobileos.server.health;
 
+import static com.libremobileos.health.HealthInterface.MODE_NONE;
+import static com.libremobileos.health.HealthInterface.MODE_AUTO;
+import static com.libremobileos.health.HealthInterface.MODE_MANUAL;
+import static com.libremobileos.health.HealthInterface.MODE_LIMIT;
+
 import static java.time.format.FormatStyle.SHORT;
 
 import android.app.AlarmManager;
@@ -40,7 +45,11 @@ import android.util.Log;
 
 import com.android.internal.R;
 
+import android.provider.Settings;
 import com.libremobileos.providers.LMOSettings;
+
+import vendor.lineage.health.ChargingControlSupportedMode;
+import vendor.lineage.health.IChargingControl;
 
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
@@ -52,16 +61,6 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-
-import android.provider.Settings;
-
-import vendor.lineage.health.ChargingControlSupportedMode;
-import vendor.lineage.health.IChargingControl;
-
-import static com.libremobileos.health.HealthInterface.MODE_NONE;
-import static com.libremobileos.health.HealthInterface.MODE_AUTO;
-import static com.libremobileos.health.HealthInterface.MODE_MANUAL;
-import static com.libremobileos.health.HealthInterface.MODE_LIMIT;
 
 public class ChargingControlController extends LineageHealthFeature {
     private final IChargingControl mChargingControl;

NurKeinNeid/frameworks_derpfest

services/java/org/derpfest/server/display/DisplayHardwareController.java
@@ -30,14 +30,14 @@ import android.util.MathUtils;
 import android.util.Slog;
 import android.view.animation.LinearInterpolator;
 
+import org.derpfest.providers.DerpFestSettings;
+import org.derpfest.hardware.LineageHardwareManager;
+import org.derpfest.hardware.LiveDisplayManager;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
-
-import org.derpfest.providers.DerpFestSettings;
-import org.derpfest.hardware.LineageHardwareManager;
-import org.derpfest.hardware.LiveDisplayManager;
 
 public class DisplayHardwareController extends LiveDisplayFeature {
 

services/java/org/derpfest/server/display/LiveDisplayFeature.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,11 @@
  */
 package org.derpfest.server.display;
 
+import static org.derpfest.server.display.LiveDisplayService.ALL_CHANGED;
+import static org.derpfest.server.display.LiveDisplayService.DISPLAY_CHANGED;
+import static org.derpfest.server.display.LiveDisplayService.MODE_CHANGED;
+import static org.derpfest.server.display.LiveDisplayService.TWILIGHT_CHANGED;
+
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;
 import android.os.Handler;
@@ -26,11 +31,6 @@ import org.derpfest.server.display.LiveDisplayService.State;
 import org.derpfest.server.display.TwilightTracker.TwilightState;
 
 import java.util.BitSet;
-
-import static org.derpfest.server.display.LiveDisplayService.ALL_CHANGED;
-import static org.derpfest.server.display.LiveDisplayService.DISPLAY_CHANGED;
-import static org.derpfest.server.display.LiveDisplayService.MODE_CHANGED;
-import static org.derpfest.server.display.LiveDisplayService.TWILIGHT_CHANGED;
 
 public abstract class LiveDisplayFeature extends LineageBaseFeature {
 

services/java/org/derpfest/server/display/LiveDisplayService.java
@@ -16,6 +16,10 @@
  */
 package org.derpfest.server.display;
 
+import static org.derpfest.hardware.LiveDisplayManager.MODE_FIRST;
+import static org.derpfest.hardware.LiveDisplayManager.MODE_LAST;
+import static org.derpfest.hardware.LiveDisplayManager.MODE_OFF;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -42,6 +46,12 @@ import org.derpfest.server.common.UserContentObserver;
 import org.derpfest.server.display.TwilightTracker.TwilightListener;
 import org.derpfest.server.display.TwilightTracker.TwilightState;
 
+import org.derpfest.app.LineageContextConstants;
+import org.derpfest.providers.DerpFestSettings;
+import org.derpfest.hardware.HSIC;
+import org.derpfest.hardware.ILiveDisplayService;
+import org.derpfest.hardware.LiveDisplayConfig;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -49,16 +59,6 @@ import java.util.BitSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-
-import org.derpfest.app.LineageContextConstants;
-import org.derpfest.providers.DerpFestSettings;
-import org.derpfest.hardware.HSIC;
-import org.derpfest.hardware.ILiveDisplayService;
-import org.derpfest.hardware.LiveDisplayConfig;
-
-import static org.derpfest.hardware.LiveDisplayManager.MODE_FIRST;
-import static org.derpfest.hardware.LiveDisplayManager.MODE_LAST;
-import static org.derpfest.hardware.LiveDisplayManager.MODE_OFF;
 
 /**
  * LiveDisplay is an advanced set of features for improving

services/java/org/derpfest/server/health/ChargingControlController.java
@@ -5,6 +5,11 @@
 
 package org.derpfest.server.health;
 
+import static org.derpfest.health.HealthInterface.MODE_NONE;
+import static org.derpfest.health.HealthInterface.MODE_AUTO;
+import static org.derpfest.health.HealthInterface.MODE_MANUAL;
+import static org.derpfest.health.HealthInterface.MODE_LIMIT;
+
 import static org.derpfest.server.health.Util.getTimeMillisFromSecondOfDay;
 import static org.derpfest.server.health.Util.msToString;
 
@@ -23,22 +28,17 @@ import android.text.format.DateUtils;
 import android.util.Log;
 
 import com.android.internal.R;
+
 import org.derpfest.server.health.ccprovider.ChargingControlProvider;
 import org.derpfest.server.health.ccprovider.Deadline;
 import org.derpfest.server.health.ccprovider.Toggle;
 
-import org.derpfest.providers.DerpFestSettings;
-
-import java.io.PrintWriter;
-
 import android.provider.Settings;
+import org.derpfest.providers.DerpFestSettings;
 
 import vendor.lineage.health.IChargingControl;
 
-import static org.derpfest.health.HealthInterface.MODE_NONE;
-import static org.derpfest.health.HealthInterface.MODE_AUTO;
-import static org.derpfest.health.HealthInterface.MODE_MANUAL;
-import static org.derpfest.health.HealthInterface.MODE_LIMIT;
+import java.io.PrintWriter;
 
 public class ChargingControlController extends LineageHealthFeature {
     private final IChargingControl mChargingControl;